3263d485d4ae7a46fd7a25483611d258d478d18e
xuzw <xzw@ebidding.net.cn>
2019-09-22 01:53:25 +0800

    新增demo

---
+++ springbootdemo/src/main/java/com/xuzw/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,15 @@
+package com.xuzw.springbootdemo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.xuzw"})
+@MapperScan("com.xuzw")
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+	    SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+}
