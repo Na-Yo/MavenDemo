41fa4bac0eabef503d999b1c5066b7848d793b84
xuzw <xzw@ebidding.net.cn>
2019-09-30 00:10:04 +0800

    集成mybatis插件生成对应实体类和xml文件

--- springbootdemo/src/main/java/com/xuzw/springbootdemo/SpringbootdemoApplication.java
+++ springbootdemo/src/main/java/com/xuzw/springbootdemo/SpringbootdemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.xuzw"})
-@MapperScan("com.xuzw")
+@MapperScan("com.xuzw.demo.dao.mapper")
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
