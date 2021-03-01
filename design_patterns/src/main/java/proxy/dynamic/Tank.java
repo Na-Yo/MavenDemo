94fd887e3e2e9545ad5f82d219b8acbac2547815
xuziwen <1218620106@qq.com>
2021-03-01 21:46:59 +0800

    动态代理

--- design_patterns/src/main/java/proxy/dynamic/Tank.java
+++ design_patterns/src/main/java/proxy/dynamic/Tank.java
@@ -1,5 +1,9 @@
 package proxy.dynamic;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -8,7 +12,7 @@ import java.util.Random;
 /**
  * @author xuziwen
  * @date 2021/2/28 10:10 下午
- * 静态代理
+ * 动态代理 借助了ASM这个非常强大的Java字节码生成框架
  */
 public class Tank implements Movable {
 
@@ -22,7 +26,23 @@ public class Tank implements Movable {
         }
     }
 
+    public void stop() {
+        System.out.println("tank move stop.....");
+        try {
+            Thread.sleep(new Random().nextInt(1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
+        cglibDynamicProxy();
+    }
+
+    /**
+     * jdk动态代理 必须实现接口
+     */
+    public static void jdkDynamicProxy(){
         Tank tank = new Tank();
         System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
         //reflection 反射 通过分析类的属性和方法
@@ -39,6 +59,14 @@ public class Tank implements Movable {
                 new Class[]{Movable.class}, new LogHandler(tank));
         movable.move();
     }
+
+    public static void cglibDynamicProxy(){
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Tank.class);
+        enhancer.setCallback(new LogInterceptor());
+        Tank tank = (Tank)enhancer.create();
+        tank.stop();
+    }
 }
 
 class LogHandler implements InvocationHandler{
@@ -54,6 +82,17 @@ class LogHandler implements InvocationHandler{
         System.out.println("method" + method.getName() +" start....");
         Object invoke = method.invoke(movable, args);
         System.out.println("method" + method.getName() +" end....");
+        return invoke;
+    }
+}
+
+class LogInterceptor implements MethodInterceptor{
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("调用："+method.getName()+"------before");
+        Object invoke = methodProxy.invokeSuper(o, objects);
+        System.out.println("调用："+method.getName()+"------after");
         return invoke;
     }
 }
